6f11472f53712e54c6fcc02ff3a16fdb30cab00f
Yuri Edward <yuri6037@outlook.com>
2026-06-27 13:24:00 +0200

    Added installer library

---
+++ installer/src/lib.rs
@@ -0,0 +1,49 @@
+// Copyright (c) 2026, BlockProject 3D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use std::ffi::{c_char, CStr};
+
+mod core;
+
+#[repr(C)]
+pub struct Installer {
+    pub name: *const c_char,
+    pub version: *const c_char,
+    pub package_len: usize,
+    pub package: *const u8
+}
+
+#[allow(unsafe_op_in_unsafe_fn)]
+#[unsafe(no_mangle)]
+unsafe extern "C" fn run_installer(config: *const Installer) {
+    let config = &*config;
+    let name = CStr::from_ptr(config.name).to_str().unwrap_unchecked();
+    let version = CStr::from_ptr(config.version).to_str().unwrap_unchecked();
+    let package = std::slice::from_raw_parts(config.package, config.package_len);
+    core::Installer::new().version(version).name(name).package(package).run();
+}
