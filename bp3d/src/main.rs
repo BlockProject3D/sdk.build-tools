ddeb68a3efa2ef7ed6c23029679137a0824bf1a6
BP3D Code Format <bp3d-format@gitlab.com>
2026-07-31 12:56:22 +0000

    Formatted code

--- bp3d/src/main.rs
+++ bp3d/src/main.rs
@@ -48,7 +48,8 @@ fn main() {
         path: args.root.as_deref().unwrap_or(Path::new("./")),
         targets: &targets,
         configuration: args.configuration.as_deref().unwrap_or("debug"),
-        features: if args.all_features.unwrap_or(true) { // By default enable all features
+        features: if args.all_features.unwrap_or(true) {
+            // By default enable all features
             Features::All
         } else {
             Features::List(&features)
